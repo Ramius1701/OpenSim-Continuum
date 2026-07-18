6d4e90bbabed261dc21a64aff42ad4b8bb915dc6
Ramius1701 <Ramius.Easterwood@gmail.com>
2026-07-19 10:28:54 +1200

    Fix pathfinding constants and file-system imports
    Add the System.IO namespace required by the Experience-Lite key-value store and define the GCNP_RADIUS and PURSUIT_OFFSET constants used by the pathfinding implementation.

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -51,6 +51,7 @@ using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
