b0b1704e8078caa4d0acf8099c12e57fbd3b5a9b
Ramius1701 <Ramius.Easterwood@gmail.com>
2026-08-01 21:09:09 +1200

    Preserve Display Names in region user cache

--- OpenSim/Region/CoreModules/Framework/UserManagement/UserManagementModule.cs
+++ OpenSim/Region/CoreModules/Framework/UserManagement/UserManagementModule.cs
@@ -1089,6 +1089,8 @@ namespace OpenSim.Region.CoreModules.Framework.UserManagement
             user.IsUnknownUser = false;
             user.HasGridUserTried = true;
             user.IsLocal = local;
+            user.DisplayName = account.DisplayName;
+            user.NameChanged = Utils.UnixTimeToDateTime(account.NameChanged);
             m_userCacheByID.Add(id, user, local ? LOCALEXPIRE : HGEXPIRE);
         }
 
